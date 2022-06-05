6b3bf93603db345043491490fa5c14d9473fb1f0
Skukzky <rifkioskar1@gmail.com>
2022-06-05 10:38:20 +0700

    challenge-callback

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrayNama, fnCallback) => {
+  let hasilSort = fnCallback(arrayNama);
+  let hasilSorter = [];
+
+  for (let nama = 0; nama < hasilSort.length; nama++) {
+    hasilSorter.push(nama + 1 + ". " + hasilSort[nama]);
+  }
+  return hasilSorter;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrCall) => {
+  return arrCall.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrCall) => {
+  return arrCall.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
